22d99a23fc7892ad4db837f8d5e1f16d1e2a5a65
Katsoumo <115957273+Katsoumo@users.noreply.github.com>
2024-12-23 17:29:42 +0100

    Update principal.c

--- codeC/principal.c
+++ codeC/principal.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>  
+#include <sys/stat.h> 
+#include <sys/types.h>
 
-#include "traitement.h"
+#include "arbre.h"
 #include "parcours.h"
+#include "traitement.h"
 
 int main(int argc, char *argv[]) {
-    /*if (argc != 4) {
-        printf("Trop d'arguments ou pas assez ! Il doit y en avoir exactement 3 !!\n");
-        exit(1);
-    }*/
-
-    const char *nom_fichier = argv[1];
-    printf("%s", argv[1]);
-    const char *fichier_final = argv[2];
-    PAVL arbre = NULL;
-    int hauteur = 0;
-
-    traiterFichier(nom_fichier, &arbre, &hauteur);
-
-    FILE *fichier = fopen(fichier_final, "w");
-    if (fichier == NULL) {
-        printf("Impossible d'ouvrir le fichier %s en écriture.\n", fichier_final);
-        exit(1);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <fichier_input>\n", argv[0]);
+        return 1;
     }
 
-    remplirCSV(arbre, fichier);
+    const char *fichierInput = argv[1];
+    const char *dossierTmp = "../tmp";        
+    const char *fichierOutput = "../tmp/output.csv"; 
 
-    fclose(fichier);
+    
+    if (access(dossierTmp, F_OK) != 0) {
+        if (mkdir(dossierTmp, 0777) != 0) {
+            perror("Erreur : Impossible de créer le dossier tmp");
+            return 3;
+        }
+    }
+
+    PAVL arbreAVL = NULL;  
+    int hauteur = 0;       
+
+   
+    traiterFichier(fichierInput, &arbreAVL, &hauteur);
+
+    if (!arbreAVL) {
+        fprintf(stderr, "Erreur : L'arbre AVL est vide après le traitement du fichier.\n");
+        return 2;
+    }
+
+    
+    creerCSV(arbreAVL, fichierOutput);
+
+    printf("Les données triées ont été exportées dans : %s\n", fichierOutput);
     return 0;
 }
+
